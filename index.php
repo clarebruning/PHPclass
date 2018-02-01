29c4216a40ba34931249a92d0ae94c1b5799c0fd
clarebruning <clarebruning@gmail.com>
2018-02-01 13:25:10 -0600

    New CSS, HTML and images for homepage

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Clare's Homepage</title>
+    <link rel="stylesheet" type="text/css" href="css/base.css">
+</head>
+
+<body>
+
+    <header>
+        <h1>Clare's Website</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="/">Homepage</a></li>
+            <li><a href="#">Loop</a></li>
+            <li><a href="#">Countdown</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <img src="images/clare.jpg" alt="photo of Clare" id="img1">
+        <p>Clare is a web developer, content creator, avid reader, writer, walker and grocery shopper. She is always rockin’ the topknot and drinking too much tea. She earned a Bachelor of Arts in English from Lawrence University in 2016, and is currently pursuing an Associate of Applied Science in Web Design & Development.</p>
+        <img src="images/topknot.png" alt="topknot logo" id="logo">
+    </main>
+
+    <footer>&copy; 2018 topknotclare.com</footer>
+
+</body>
+
+</html>
